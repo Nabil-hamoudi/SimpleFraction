2c0b5e9421ac06171513354d2da88e41cd8c1d28
--global <nabil.hamoudi@ens.uvsq.fr>
2023-10-18 16:02:18 +0200

    Question 1,2 partie II

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,30 @@
 public class Fraction {
-	public String toString() { return "je suis une fraction"; }
+  // Declaration des attributs
+  private int numerateur;
+  private int denominateur;
+	
+  // Constructeurs
+  public InitNumerateurOnly (int numerateur) {
+    this.numerateur = numerateur;
+    this.denominateur = 1;
+  }
+
+  public InitNumerateurDenominateur (int numerateur, int denominateur) {
+    this.numerateur = numerateur;
+    this.denominateur = denominateur;
+  }
+
+  public InitEmpty () {
+    this.numerateur = 1;
+    this.denominateur = 1;
+  }
+
+  // Fonction recuperation attributs
+  public int GetNumerateur () {
+    return numerateur;
+  }
+ 
+  public int GetDenominateur () {
+    return numerateur;
+  }
 }
